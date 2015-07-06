c1a3aad7ed2afb66c0d3797337a9b3b34974afcc
Vlad <vlad@v-lad.org>
2015-07-06 00:36:48 -0400

    Win7 fixes

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-BOX=vladistan/win7
+export VM=win7
+export BOX=vladistan/$VM
 
 vagrant init vladistan/win7
 vagrant box update --provider vmware_fusion 
 vagrant up --provider vmware_fusion
+vagrant halt
 
 export VMX_NAME=`cat .vagrant/machines/default/vmware_fusion/id`
 echo $VMX_NAME
 
-
+packer build vsphere.json
